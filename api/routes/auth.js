fe36dc3082c0d0ce3c58ebdecb9f74dd431cfad5
LiYuanzhi <liyuanzhi@LiYuanzhideMacBook-Pro.local>
2022-12-05 22:06:06 -0800

    first commit

---
+++ api/routes/auth.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { register } from "../controllers/auth.js";
+import { login } from "../controllers/auth.js";
+
+
+
+const router = express.Router();
+
+router.post("/register", register);
+router.post("/login", login);
+ 
+
+// module.exports=router;
+
+export default router
